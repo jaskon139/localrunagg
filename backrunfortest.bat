82b9c9f87259c7cdef572907ac92ffee7d064f70
jaskon139 <jaskon139@gmail.com>
2024-01-15 18:02:11 +0800

    hell777o

--- backrunfortest.bat
+++ backrunfortest.bat
@@ -10,7 +10,7 @@ TASKKILL /F /IM clash.exe
 TASKKILL /F /IM dnscrypt-proxy.exe
 
 rem aggtunnel
-start /MIN /B c:\opensource\aggligator-util-v0.11.0\target\debug\agg-tunnel.exe client -g --port 22:4301 --port 1194:4194 --tcp 192.168.43.1:7002 --tcp 192.168.44.1:7003 --tcp 192.168.45.1:15959 --tcp 172.24.48.1:7001 -n
+start /MIN /B c:\opensource\aggligator-util-v0.11.0\target\debug\agg-tunnel.exe client -g --port 22:4301 --port 1194:4194 --tcp 192.168.43.1:7002 --tcp 192.168.44.1:7003 --tcp 192.168.45.1:15959 --tcp 192.168.46.1:7005 -n
 
 rem gost 
 rem start /MIN /B C:\opensource\gost\gost-windows-amd64.exe -L=:23232 -L=tcp://:7001/jaskon139-junjieh7.koyeb.app:443 -L=tcp://:7002/172.66.44.208:443 -L=tcp://:7003/vm2jaskon139.dynv6.net:4949 -F=http://127.0.0.1:7890  >nul 2>nul
@@ -26,6 +26,7 @@ rem xray
 rem start /MIN /B  C:\tools\v2ray\4.21.3\v2ray.exe run -c .\xray\jaskon139-junjieh7koyebapp.json
 start /MIN /B  C:\tools\v2ray\4.21.3\v2ray.exe run -c .\xray\vleascloudflare.json
 start /MIN /B  C:\tools\v2ray\4.21.3\v2ray.exe run -c .\xray\vm2jaskon139.json
+start /MIN /B  C:\tools\v2ray\4.21.3\v2ray.exe run -c .\xray\jaskon139-junjieh7koyebapp.json
 rem start /MIN /B  C:\tools\Netch\Netch.exe
 start /MIN /B  C:\opensource\clash\clash.exe -d .\clashconfig
 
@@ -83,3 +84,18 @@ rem 中国 广东 广州 联通157.255.78.51
 rem 183.232.233.5
 
 rem 157.0.148.53
+
+rem          220.181.33.174
+rem          110.242.70.68
+rem          163.177.17.6
+rem          183.240.98.84
+rem          110.242.70.69
+rem          14.215.182.75
+rem          36.155.169.188
+rem          157.0.148.53
+rem          163.177.17.189
+rem          153.3.237.117
+rem          180.101.50.249
+rem          180.101.50.208
+rem          220.181.7.1
+rem          220.181.111.189
